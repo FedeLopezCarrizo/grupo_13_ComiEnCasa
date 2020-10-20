fef1d5daf97616e028f5afe3f1b24d187202a188
FedeLopezCarrizo <flopezcarrizo@gmail.com>
2020-10-20 14:36:09 -0300

    edit product and responsive

--- comiEnCasa/src/controllers/productsController.js
+++ comiEnCasa/src/controllers/productsController.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsfilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsfilePath, { encoding:'utf-8' } ));
+
 const controller = {
 	store: (req, res) => {
 		res.render('products/productCart');
@@ -5,6 +11,20 @@ const controller = {
 	
 	detail: (req, res) => {
 		res.render('products/productDetail');
+	}, 
+	edit: (req, res) => {
+		let idProduct = req.params.idProduct;
+
+		/* if (fileProduct != ""){
+			product = JSON.parse(fileProduct);
+		} */
+
+		let productToEdit = products[idProduct];
+
+		res.render('products/productEdit', { productToEdit: productToEdit });
+	}, 
+	update: (req, res, next) => {
+		res.render(req.body);
 	}
 };
 
--- comiEnCasa/src/routes/products.js
+++ comiEnCasa/src/routes/products.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 
 /* Controller Require */
 const productsController = require('../controllers/productsController');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '../../public/images'))
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+  })
+   
+var upload = multer({ storage: storage })
 
 router.get('/store', productsController.store); 
 
 /* Get one product */
 router.get('/detail', productsController.detail);
+
+router.get('/edit/:idProduct', productsController.edit);
+
+router.put('/edit', upload.any(), productsController.update);
 
 module.exports = router;
