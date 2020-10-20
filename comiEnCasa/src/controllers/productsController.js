95b5baad744e561ad26f7cd587e500f7450f6aa8
Diego250987 <diego.hernan.diaz@hotmail.com>
2020-10-20 14:51:40 -0300

    se agrego NewProduct ejs y css, se agrego ruta

--- comiEnCasa/src/controllers/productsController.js
+++ comiEnCasa/src/controllers/productsController.js
@@ -1,3 +1,5 @@
+let fs = require('fs');
+
 const controller = {
 	store: (req, res) => {
 		res.render('products/productCart');
@@ -5,7 +7,22 @@ const controller = {
 	
 	detail: (req, res) => {
 		res.render('products/productDetail');
+	},
+	NewProduct: (req, res) => {
+		let nuevoProducto = {
+			tituloProducto: req.body.tituloProducto,
+			Descripcion: req.body.Descripcion,
+			tipo: req.body.tipo,
+			precio: req.body.precio,
+		}
+
+		let nuevoProductoJSON = JSON.stringify(nuevoProducto);
+
+		fs.writeFileSync('nuevoProducto.JSON',nuevoProductoJSON);
+
+		res.render('products/NewProduct');
 	}
+
 };
 
 module.exports = controller;
--- comiEnCasa/src/routes/products.js
+++ comiEnCasa/src/routes/products.js
@@ -9,4 +9,6 @@ router.get('/store', productsController.store);
 /* Get one product */
 router.get('/detail', productsController.detail);
 
+router.get('/NewProduct', productsController.NewProduct)
+
 module.exports = router;
